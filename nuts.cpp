7e180f9124624c8a7c66365889503dec9d3487f7
Nick MACHNIK <nmachnik@lngrad182.ist.local>
2021-08-23 20:31:12 +0200

    feat: NUTS impl left off at build tree step

---
+++ nuts.cpp
@@ -0,0 +1,117 @@
+#include <math.h>
+#include <random>
+#include <chrono>
+#include "nuts.h"
+
+NUTS::NUTS(double initial_position,
+           double sigma,
+           int burn_in_iterations) : sigma{sigma},
+                                     burn_in_iterations{burn_in_iterations} {};
+
+// logarithm of the target density function
+double NUTS::log_target_density(double position)
+{
+    return -0.5 * position * position;
+}
+
+// gradient of the target density function
+double NUTS::target_gradient(double position)
+{
+    return -position;
+}
+
+// logarithm of the density function of the auxiliary momentum
+double NUTS::log_momentum_density(double momentum)
+{
+    return -0.5 * momentum * momentum;
+}
+
+// joint density of position and momentum
+double NUTS::joint_density(state w)
+{
+    return exp(log_momentum_density(w.momentum) + log_target_density(w.position));
+}
+
+state NUTS::leapfrog(state w)
+{
+    w.momentum += 0.5 * step_size * target_gradient(w.position);
+    w.position += step_size * w.momentum;
+    w.momentum += 0.5 * step_size * target_gradient(w.position);
+
+    return w;
+}
+
+bool NUTS::step_size_is_reasonable(state w_old, state w_new, double a)
+{
+    return pow((joint_density(w_new) / joint_density(w_old)), a) <= pow(2, -a);
+}
+
+// see Hoffman and Gelman (2014)
+void NUTS::find_reasonable_step_size(state w)
+{
+    state w_new{leapfrog(w)};
+    double a{2 * ((joint_density(w_new) / joint_density(w)) > 0.5) - 1};
+    while (!step_size_is_reasonable(w, w_new, a))
+    {
+        step_size *= pow(2, a);
+        w_new = leapfrog(w);
+    }
+}
+
+double NUTS::sample_slice_threshold(state w)
+{
+    double upper_bound{
+        exp(
+            log_target_density(w.position) +
+            log_momentum_density(w.momentum))};
+
+    std::uniform_real_distribution<double> u_dist(0.0, upper_bound);
+
+    return u_dist(rnd_generator);
+}
+
+double NUTS::sample_direction()
+{
+    double c{standard_normal(rnd_generator)};
+
+    if (c >= 0)
+    {
+        return direction::right;
+    }
+
+    return direction::left;
+}
+
+double NUTS::single_sample(double initial_position)
+{
+    if (!burned_in)
+    {
+    }
+
+    // r
+    double momentum{sample_momentum()};
+    // w (theta, r)
+    state w{initial_position, momentum};
+    // u
+    double slice_threshold{sample_slice_threshold(w)};
+
+    // theta^(-)
+    double leftmost_position{w.momentum};
+    // theta^(+)
+    double rightmost_position{w.momentum};
+
+    // s
+    bool continue_integration{true};
+    while (continue_integration)
+    {
+        // v
+        double v{sample_direction()};
+
+        if (v == direction::left)
+        {
+        }
+        else
+        {
+        }
+    }
+}
---
+++ nuts.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <vector>
+#include <random>
+#include <chrono>
+
+// a position-momentum state
+struct state
+{
+    double position;
+    double momentum;
+
+    state(double position, double momentum) : position{position}, momentum{momentum} {};
+};
+
+enum direction
+{
+    left = -1,
+    right = 1,
+};
+
+// A No-U-Turn Sampler with Dual Averaging.
+// Featuring dynamic integration length and automatic step size selection.
+class NUTS
+{
+private:
+    const double gamma{0.05};
+    const double t_0{10.0};
+    const double kappa{0.75};
+
+    std::mt19937 rnd_generator{std::chrono::steady_clock::now().time_since_epoch().count()};
+    std::normal_distribution<double> standard_normal{0.0, 1.0};
+    double step_size{1};
+    bool burned_in{false};
+    double mu;
+    double sigma;
+    double H;
+    int burn_in_iterations;
+
+    double sample_momentum();
+    double sample_direction();
+    double sample_slice_threshold(state w);
+    state leapfrog(state w);
+    bool step_size_is_reasonable(state w_old, state w_new, double alpha);
+    void find_reasonable_step_size(state w);
+
+    double joint_density(state w);
+    double target_gradient(double position);
+    double log_target_density(double position);
+    double log_momentum_density(double momentum);
+
+public:
+    NUTS(double initial_position, double sigma, int burn_in_iterations);
+    double single_sample(double initial_position);
+};
